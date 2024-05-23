0f7d7230cda745908511734db9fbe0ae796d3c00
naya0000 <naya60135@gmail.com>
2024-05-24 02:57:57 +0800

    add comments

--- examples/application/main.go
+++ examples/application/main.go
@@ -1,27 +1,31 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/if-nil/tcc-toy/pb"
 	"github.com/if-nil/tcc-toy/transaction_manager"
-	"log"
 )
 
 func main() {
+	// Transaction Manager is like a Coordinator in Two Phase Commit (2PC) protocol
 	err := transaction_manager.TCCCall(func(tx *transaction_manager.Transaction) transaction_manager.Operation {
+		// CallTry is like the first phase in 2PC: Request To Prepare
 		_, err := tx.CallTry("localhost:9210", &pb.TryRequest{Param: "tx1"})
+		// If any error occurs, the transaction will be cancelled
 		if err != nil {
-			log.Printf("tx1 try failed: %v", err)
+			fmt.Printf("tx1 try failed: %v", err)
 			return tx.Cancel()
 		}
 		_, err = tx.CallTry("localhost:9211", &pb.TryRequest{Param: "tx2"})
 		if err != nil {
-			log.Printf("tx2 try failed: %v", err)
+			fmt.Printf("tx2 try failed: %v", err)
 			return tx.Cancel()
 		}
-		log.Printf("tx1 and tx2 try success")
+		fmt.Printf("tx1 and tx2 try success")
 		return tx.Commit()
 	})
 	if err != nil {
-		log.Fatalf("transaction failed: %v", err)
+		fmt.Errorf("transaction failed: %v", err)
 	}
 }
